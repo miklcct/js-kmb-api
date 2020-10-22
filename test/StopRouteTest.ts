60ad3aff39f528c54eeffd0d8d532088c432c55f
Michael Tsang <miklcct@gmail.com>
2020-10-22 16:21:01 +0800

    add more tests for ETA

--- test/StopRouteTest.ts
+++ test/StopRouteTest.ts
@@ -170,7 +170,7 @@ export class StopRouteTest extends TestCase {
     ) : Promise<void> {
         this.clock = Sinon.useFakeTimers(new Date(now));
         Sinon.stub(Secret, 'getSecret')
-            .returns(new Secret('A06F1CC2A3A43BD8B7A80846F7D65501AE1503A9&ctr=1043206738', 1043206738));
+            .returns(new Secret('A06F1CC2A3A43BD8B7A80846F7D65501AE1503A9', 1043206738));
         const http_stub = Sinon.stub(Axios, 'get').returns(
             Promise.resolve(
                 {
@@ -211,7 +211,7 @@ export class StopRouteTest extends TestCase {
                         stop_seq : '10',
                         service_type : '1',
                         vendor_id : Secret.VENDOR_ID,
-                        apiKey : 'A06F1CC2A3A43BD8B7A80846F7D65501AE1503A9&ctr=1043206738',
+                        apiKey : 'A06F1CC2A3A43BD8B7A80846F7D65501AE1503A9',
                         ctr : '1043206738',
                     },
                     responseType : 'json'
@@ -238,6 +238,69 @@ export class StopRouteTest extends TestCase {
         const {http_stub, error, stop_route} = StopRouteTest.setUpFailureCalls();
         await assert.isRejected(stop_route.getEtas(2, 'GET'), error);
         assert.strictEqual(http_stub.callCount, 3);
+    }
+
+    @test
+    async getEtasWithPost() : Promise<void> {
+        this.clock = Sinon.useFakeTimers(new Date('2020-10-18T14:21:57+08:00'));
+        const secret_stub = Sinon.stub(Secret, 'getSecret');
+        secret_stub.returns(new Secret('A06F1CC2A3A43BD8B7A80846F7D65501AE1503A9', 1043206738));
+        const data = new Secret('8551C38904BF72BEF85A43EBF8F40D02BDBBD800', 1043206738);
+        secret_stub.withArgs(
+            `?${
+                new URLSearchParams(
+                    {
+                        lang : 'en',
+                        route : '960',
+                        bound : '2',
+                        stop_seq : '10',
+                        service_type : '1',
+                        vendor_id : Secret.VENDOR_ID,
+                        apiKey : 'A06F1CC2A3A43BD8B7A80846F7D65501AE1503A9',
+                        ctr : '1043206738',
+                    }
+                )
+                    .toString()
+            }`
+        )
+            .returns(data);
+        const http_stub = Sinon.stub(Axios, 'post').returns(
+            Promise.resolve(
+                {
+                    data : [
+                        {
+                            routeNo : '960',
+                            bound : 2,
+                            service_type : 1,
+                            seq : 10,
+                            responsecode : 0,
+                            updated : 1603346890000,
+                            generated : 1603346896767,
+                            eta : []
+                        }
+                    ],
+                    status : 200,
+                    statusText : 'OK',
+                    headers : {},
+                    config : {}
+                }
+            )
+        );
+        const kmb = new Kmb();
+        const stop_route = new kmb.StopRoute(
+            new kmb.Stop('WE01-N-1250-0', 'Western Harbour Crossing', 'B', 10)
+            , new kmb.Variant(new kmb.Route('960', 2), 1, 'Wan Chai North', 'Tuen Mun (Kin Sang Estate)', '')
+            , 10
+        );
+        const results = await stop_route.getEtas(5, 'POST');
+        assert(
+            http_stub.calledWith(
+                'https://etav3.kmb.hk/?action=geteta'
+                , {d : data.apiKey, ctr : data.ctr}
+                , {responseType : 'json'}
+            )
+        );
+        assert.deepStrictEqual(results, []);
     }
 
     private static setUpFailureCalls() : {http_stub : SinonStub, error : Error, stop_route : StopRoute} {
